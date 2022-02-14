e1bfbbb0ffff7e75c6890e76b18e9849436c1560
Mohamed Maskoul <66232435+maskoul-mohamed@users.noreply.github.com>
2022-02-14 12:23:25 +0000

    Add header

---
+++ index.php
@@ -0,0 +1,6 @@
+<!DOCTYPE html>
+
+<html>
+    <?php   include_once 'templates/header.php'; ?>
+
+</html>
---
+++ templates/header.php
@@ -0,0 +1,15 @@
+<head>
+    <title>Restaurant</title>
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+</head>
+
+<body>
+<nav class="navbar navbar-light bg-light">
+  <div class="container-fluid d-flex justify-content-end">
+    <a class="btn btn-lg btn-primary " href="add.php">Add To Menu</a>
+  </div>
+</nav>
+</body>
