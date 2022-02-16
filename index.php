016510a1c0e78c1cb6ad09624eeea318e132023d
Mohamed Maskoul <66232435+maskoul-mohamed@users.noreply.github.com>
2022-02-16 12:41:55 +0000

    Add rendering data to browser

--- index.php
+++ index.php
@@ -30,7 +30,24 @@
 <html>
 <?php   include_once 'templates/header.php'; ?>
 
-   
+<h2 class="text-secondary text-center mt-4">Meals!</h2>
+
+<div class="container">
+    <div class="row">
+
+        <?php foreach($food as $meal){ ?>
+        <div class="col col-sm-6 col-md-3">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title"><?php echo htmlspecialchars($meal['name']) ?></h5>
+                    <p class="card-text"><?php echo htmlspecialchars($meal['address'])?></p>
+                    <a href="#" class="btn btn-primary text-end">more info</a>
+                </div>
+            </div>
+        </div>
+    <?php }?>
+    </div>
+</div>
 <?php include_once 'templates/footer.php' ; ?>
 
 </html>
