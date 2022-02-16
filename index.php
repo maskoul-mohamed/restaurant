5da1ff6259a1d012197d0b9383ada787e7b1c117
Mohamed Maskoul <66232435+maskoul-mohamed@users.noreply.github.com>
2022-02-16 12:40:15 +0000

    Add connecte to database & show query

--- index.php
+++ index.php
@@ -1,6 +1,36 @@
+<?php
+    // connect to database
+    $conn = mysqli_connect('localhost', 'maskoul', 'test123', 'soli_food');
+
+    // check connection
+    if(!$conn){
+        echo 'Connection error: ' . mysqli_connect_error(); 
+    }
+
+    // write query for all food
+    $sql = 'SELECT  id, name, address, createdAt from food';
+
+    // make query & get result
+    $resutl = mysqli_query($conn, $sql);
+
+    // fetch the resulting rows as an array
+    $food = mysqli_fetch_all($resutl, MYSQLI_ASSOC);
+
+    // free result from memory
+    mysqli_free_result($resutl);
+
+    // close the connection
+    mysqli_close($conn);
+
+    print_r($food);
+?>
+
 <!DOCTYPE html>
 
 <html>
-    <?php   include_once 'templates/header.php'; ?>
-    <?php include_once 'templates/footer.php' ; ?>
+<?php   include_once 'templates/header.php'; ?>
+
+   
+<?php include_once 'templates/footer.php' ; ?>
+
 </html>
